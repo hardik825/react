6528c3419c9faa0ed553eab4bc014c2546671580
Hardik Chauhan <hardikchauhan442@gmail.com>
2022-07-27 22:14:04 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+
+import Navbar from "./components/Navbar";
+import About from "./components/About ";
 
 function App() {
   return (
@@ -8,7 +10,8 @@ function App() {
       <Navbar title="TextUtils" aboutText="About TextUtils" />
       {/* <Navbar title="TextUtils" /> */}
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze" />
+        {/* <TextForm heading="Enter the text to analyze" /> */}
+        <About />
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,29 +1,57 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const handleUpClick = () => {
-    console.log("Uppercase was clicked");
+  const handleLoClick = () => {
+    // console.log("Uppercase was clicked");
+    let newText = text.toLowerCase();
+    setText(newText);
   };
-  const handleOnChange = () => {
-    console.log("on change");
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleClearClick = () => {
+    let newText = '';
+    setText(newText);
+  };
+  const handleOnChange = (event) => {
+    // console.log("on change");
+    setText(event.target.value);
   };
 
-  const [text, setText] = useState("Entere text here");
+  const [text, setText] = useState("");
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          id="mybox"
-          value={text}
-          onChange={handleOnChange}
-          rows="8"
-        ></textarea>
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="mybox"
+            value={text}
+            onChange={handleOnChange}
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+          Convert to Lovercase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleClearClick}>
+          Convert to Lovercase
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>
-        Convert to Uppercase
-      </button>
-    </div>
+      <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes Read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
   );
 }
